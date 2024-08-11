513025dd62c6f61b278510ba808576e45eb71543
emptemperor <140359833+emptemperor@users.noreply.github.com>
2024-08-11 19:55:00 -0400

    Update hardware-configuration.nix

--- nixosModules/hardware-configuration.nix
+++ nixosModules/hardware-configuration.nix
@@ -45,7 +45,7 @@
       fsType = "zfs";
     };
 
-  fileSystems."/mnt/boot" =
+  fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/93E5-F903";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
