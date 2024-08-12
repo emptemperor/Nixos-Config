87fed7ed1d18741e10507ac3ffa5d52a65735157
emptemperor <140359833+emptemperor@users.noreply.github.com>
2024-08-11 23:48:01 -0400

    Zfs file system modification

--- nixosModules/default.nix
+++ nixosModules/default.nix
@@ -5,6 +5,7 @@
  {
    imports = [
       ./configuration.nix
+      ./zfs.nix
  
    ];
  } 
---
+++ nixosModules/zfs.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  lib,
+  pkgs,
+  ... 
+}:
+{
+  boot = {
+    # booting with zfs
+    supportedFilesystem.zfs = true;
+    kernelPackages = pkgs.linuxPackages_xanmod_latest;
+       zfs = {
+        # Use by-id for intel cpu
+        devNodes =
+        "dev/disk/by-id";
+        package = pkgs.zfs_unstable;
+        requestEncryptionCredentials = cfg.encryption; 
+       };
+  }; 
+
+# 16GB swap
+swapDevices = [ { device = "/dev/disk/by-label/SWAP"; } ];
+
+
+
+}
