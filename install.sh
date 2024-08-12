788fa510a90e2cdd9eccd52f1b925f2d28aca827
emptemperor <140359833+emptemperor@users.noreply.github.com>
2024-08-11 22:00:04 -0400

    Removed generation of hardware

--- install.sh
+++ install.sh
@@ -143,8 +143,6 @@ else
 fi
 sudo mount --mkdir -t zfs zroot/persist /mnt/persist
 
-# Manually Generate Hardware after Moutning
-sudo nixos-generate-config --no-filesystems --root /mnt
 
 read -rp "Enter git rev for flake (default: main): " git_rev
 echo "Installing NixOS"
