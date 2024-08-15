5093e7e65d27dc79f864057284e4c6b7364c279c
emptemperor <140359833+emptemperor@users.noreply.github.com>
2024-08-15 19:37:55 -0400

    Added Wayland to SDDM

--- nixosModules/configuration.nix
+++ nixosModules/configuration.nix
@@ -63,7 +63,12 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm = {
+     enable = true;
+     wayland.enable = true;
+     
+  };
+  
   services.desktopManager.plasma6.enable = true;
   
 
