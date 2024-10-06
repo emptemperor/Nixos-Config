54765d15aa63ac969ea518761cb9fd2efe281074
emptemperor <140359833+emptemperor@users.noreply.github.com>
2024-10-06 14:41:13 -0400

    revert

--- homeModules/default.nix
+++ homeModules/default.nix
@@ -11,27 +11,4 @@
     ];
 
 
-<<<<<<< HEAD
-=======
-    };
-
-    config = {
-
-    hyprland = lib.mkIf config.hyprland.enable {
-      dependencies = lib.mkDefault true;
-      hyprpaper = lib.mkDefault true;
-      settings = lib.mkDefault true;
-    };
-
-    terminal = lib.mkIf config.terminal.enable {
-      kitty = lib.mkDefault true;	
-    };
-
-    modules = lib.mkIf config.modules.enable {
-      packages = lib.mkDefault true;
-      programs = lib.mkDefault true;
-    };
-
-   };	
->>>>>>> 17724f0f968fa241f7edfc6f54a51e4628fbc061
   }
